46db687056018d64f84ab77a27bfce7ce44c5d09
togunchan <togunchan@gmail.com>
2024-12-29 18:50:27 +0300

    Added user registration and login functionality, including JWT token generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,12 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+// const { BadRequestError } = require('../errors')
+const jwt = require('jsonwebtoken')
+
 const register = async (req, res) => {
-  res.send('register user')
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
